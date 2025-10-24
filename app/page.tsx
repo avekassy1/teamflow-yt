57d4917fb6c3841a07e490c2964791ef2c86e0c7
Andras <vekassy.andras@gmail>
2025-10-24 22:20:28 +0100

    Add ENTSO-E logo, download more public sources

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { HeroHeader } from "@/components/header";
+import HeroSection from "@/components/hero-section";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+
 export default function Home() {
   return (
     <div className="">
-      <h1>Hi</h1>
+      <HeroHeader />
+      <HeroSection />
     </div>
   );
 }
