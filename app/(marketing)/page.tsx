1da76e97b8754f5f88aa46763aa177d65146f6bd
Andras <vekassy.andras@gmail>
2025-10-27 17:29:44 +0000

    restructure folders

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,5 @@
-import { HeroHeader } from "@/components/header";
-import HeroSection from "@/components/hero-section";
+import { HeroHeader } from "@/app/(marketing)/_components/header";
+import HeroSection from "@/app/(marketing)/_components/hero-section";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
