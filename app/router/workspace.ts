a0c6b8bea9b00a9d5ade7c8a222f47a0f9e35d08
Andras <vekassy.andras@gmail>
2025-11-10 14:29:50 +0000

    error responses

--- app/router/workspace.ts
+++ app/router/workspace.ts
@@ -1,3 +1,5 @@
+import { KindeOrganization, KindeUser } from '@kinde-oss/kinde-auth-nextjs';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { os } from '@orpc/server';
 import z from 'zod';
 
@@ -9,10 +11,29 @@ export const listWorkspaces = os
         tags: ['workspace'],
     })
     .input(z.void())
-    .output(z.void())
+    .output(z.object({
+        workspaces: z.array(
+            z.object({
+                id: z.string(),
+                name: z.string(),
+                avatar: z.string(),
+            })
+        ),
+        user: z.custom<KindeUser<Record<string, unknown>>>(),
+        currentWorkspace: z.custom<KindeOrganization<unknown>>(),
+    }))
     .handler(async ({input}) => {
-        console.log(input);
+        const {getUserOrganizations} = getKindeServerSession() // only runs on server side
+        const organizations = await getUserOrganizations();
+
+        const workspaces = organizations?.orgs.map((org) => ({
+            id: org.code,
+            name: org.name,
+            avatar: org.name?.charAt(0) ?? 'W',
+          })) ?? [];
+        
+        return {
+            workspaces,
+        }
         
     })
-
-// HALO continue from 3hr 29 mins
